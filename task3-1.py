8b634bce6c4b6e00648c599ead3d8a326f9dd3b4
“Igor_Dorozhkin“ <Igor_Dorozhkin@epam.com>
2021-10-12 22:23:10 +0300

    add_text

--- task3-1.py
+++ task3-1.py
@@ -1,4 +1,23 @@
 #!/c/Users/AngryBear/AppData/Local/Microsoft/WindowsApps/python
+#   Create a simple, separate python app which would monitor the your system/server.
+#   Output should be written to the plain text file or json file.(needs to be specified in configuration file)
+#   For monitoring purposes use psutilmodule, see: https://pythonhosted.org/psutil/
+#   It should create snapshots of the state of the system each 5 minutes (interval should be configurable):
+#    ● Overall CPU load
+#    ● Overall memory usage
+#    ● Overall virtual memory usage
+#    ● IO information
+#    ● Network information
+#   Example of the structure for output data is about as follows:
+#   SNAPSHOT 1: TIMESTAMP : <columns for system wide data>
+#   SNAPSHOT 2: TIMESTAMP : <columns for system wide data>
+#   SNAPSHOT 3: TIMESTAMP : <columns for system wide data>
+#   Config example
+#   [common]
+#   output = json
+#   interval = 5
+#   You can use any type of configs: ini files, yaml, json or even python settings.py.
+
 import psutil
 import time 
 from datetime import datetime, date
