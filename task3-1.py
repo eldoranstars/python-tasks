2f141266b6218d9d403549ad0c78c464131338fd
“Igor_Dorozhkin“ <Igor_Dorozhkin@epam.com>
2021-10-12 22:20:11 +0300

    add_task3

--- task3-1.py
+++ task3-1.py
@@ -1,7 +1,19 @@
 #!/c/Users/AngryBear/AppData/Local/Microsoft/WindowsApps/python
 import psutil
+import time 
+from datetime import datetime, date
 
-#Overall CPU load
-print(psutil.cpu_percent(percpu=True))
-#Network information
-print(psutil.net_io_counters())
+while True:
+    print('========SNAPSHOT========')
+    print("Date: "+str(date.today().year)+"-"+str(date.today().month)+"-"+str(date.today().day))
+    print("Time: "+str(datetime.today().hour)+":"+str(datetime.today().minute)+":"+str(datetime.today().second))
+    print('Overall CPU load:', psutil.cpu_percent(1))
+    print('Overall swap memory usage:')
+    print(psutil.swap_memory())
+    print('IO information:')
+    print(psutil.disk_io_counters())
+    print('Overall virtual memory usage:')
+    print(psutil.virtual_memory())
+    print('Network information:')
+    print(psutil.net_io_counters())
+    time.sleep(10)
